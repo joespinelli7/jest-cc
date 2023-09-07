21830ad7cea0b98af370ee82b129407fda82f98b
Joe Spinelli <joe.spinelli@hidglobal.com>
2023-09-06 22:48:58 -0500

    test: Add numbers tests using .toBe >, <, >=, <= matcher methods

--- sum.test.js
+++ sum.test.js
@@ -25,3 +25,19 @@ describe("truthy or falsy", () => {
 		// expect(n).not.toBeUndefined();
 	})
 })
+
+describe("numbers", () => {
+	it("two plus two", () => {
+		const value = 2 + 2;
+		expect(value).toBe(4);
+		expect(value).toBeGreaterThan(3);
+		expect(value).toBeGreaterThanOrEqual(4);
+		expect(value).toBeLessThan(5);
+		expect(value).toBeLessThanOrEqual(4);
+	})
+
+	it("adding floats", () => {
+		const value = 0.1 + 0.2;
+		expect(value).toBeCloseTo(0.3);
+	})
+})
