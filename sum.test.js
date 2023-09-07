11c67b349e9440be6fd0b8bf97011e2d9ab79972
Joe Spinelli <joe.spinelli@hidglobal.com>
2023-09-06 22:37:43 -0500

    test: Add descrbie block and new obj test using toEqual

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,16 @@
 const sum = require("./sum");
 
-it("should add 1 + 2 to equal 3", () => {
-	const result = sum(1, 2);
-	expect(result).toBe(3);
-})
+// describe used to group common tests together
+describe("example tests", () => {
+	it("should add 1 + 2 to equal 3", () => {
+		const result = sum(1, 2);
+		expect(result).toBe(3);
+	})
 
+	it("object assignment", () => {
+		const obj = {};
+		// use toEqual over toBe here b/c you want to check the value. If comparing using toBe then test would be false b/c
+		// objects are referenced by memory. Even if they have the same properties they're different objects.
+		expect(obj).toEqual({});
+	})
+})
